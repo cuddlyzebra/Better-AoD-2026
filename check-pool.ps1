59ed40636b8b46133a2707a2b0d1713dd869fa2e
Cuddly Zebra <zebra@bazzite.com>
2026-09-13 01:27:18 +0800

    Remove debug scripts

--- check-pool.ps1
+++
@@ -1,8 +0,0 @@
-$content = Get-Content public\main.js -Raw
-$index = $content.IndexOf('Even I can not control')
-if ($index -ge 0) {
-    $start = [Math]::Max(0, $index - 60)
-    $content.Substring($start, 150)
-} else {
-    "NOT FOUND"
-}
